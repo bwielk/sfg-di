c14ef8671f918457271e1be76e226164c1bad674
bwielk <bwielk@gmail.com>
2020-03-16 19:46:46 +0100

    Added a bean

---
+++ src/main/java/dependencyinjection/DiApplication.java
@@ -0,0 +1,19 @@
+package dependencyinjection;
+
+import controllers.MyController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		myController.sayHello();
+		System.out.println(myController.sayHello());
+	}
+
+}
